be94ce7058297f9a762fd43db8e84e3e7ead5209
ddivernois <david-emmanuel.divernois@amadeus.com>
2015-03-18 15:20:36 +0100

    Issue with foreach inside switch.

--- src/hello.js
+++ src/hello.js
@@ -1,18 +1,49 @@
 import {Component, Template} from 'angular2/angular2';
-import {If} from 'angular2/angular2';
+import {Switch, SwitchWhen, SwitchDefault, Foreach} from 'angular2/angular2';
 
 @Component({
-    selector: 'hello'  //TODO: default to camel-cased class name if not provided?
+    selector: 'hello'
 })
 @Template({
-    inline: `<span *if="name">Hello, {{name}}!</span>`,
-    directives: [If]
+    inline: `
+<input type="text" (input)="onInput($event)" placeholder="Type something here"><br>
+<ul [switch]="getSwitchValue()">
+    <li *switch-when="'empty'">No suggestion</li>
+    <li *switch-when="'waiting'">Loading suggestions...</li>
+    <template [switch-default]>
+      <li *foreach="#suggestion in list">{{suggestion}}</li>
+    </template>
+</ul>
+`,
+    directives: [Switch, SwitchWhen, SwitchDefault, Foreach]
 })
 export class Hello {
     constructor() {
-        this.name = 'World'; //TODO: how to declare a field with its default value (so I don't need a constructor)?
-        setTimeout(() => {
-          this.name = 'NEW World'
-        }, 2000);
+      this.list = null;
+    }
+    onInput($event) {
+      var value = $event.target.value;
+      if (value) {
+        var promise = this.list = new Promise((resolve) => {
+          resolve(["Suggestion 1 for " + value, "Suggestion 2 for " + value]);
+        });
+        promise.then((resolvedList) => {
+          if (this.list != promise) {
+            return;
+          }
+          this.list = resolvedList;
+        });
+      } else {
+        this.list = null;
+      }
+    }
+    getSwitchValue() {
+      if (!this.list || this.list.length == 0) {
+        return "empty";
+      } else if (this.list.then) {
+        return "waiting";
+      } else {
+        return "results";
+      }
     }
 }
